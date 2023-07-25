1f7b9ff75fd7e36f4ef4487d2e74a78c9d190b31
Jose Melo <jmelo@live.cl>
2023-07-25 11:30:42 +0200

    added full bootstrap & discount examples

--- examples/apiExample.py
+++ examples/apiExample.py
@@ -1,9 +1,5 @@
 from flask import Flask, request
-import sys
-import os
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(parent_dir + '/../src')
-from curveengine import CurveEngine
+import curveengine as ce
 
 '''
     Codigo de referencia
@@ -11,20 +7,61 @@ from curveengine import CurveEngine
 app = Flask('app')
 # post
 
+
 @app.route('/bootstrap', methods=['POST'])
 def bootstrap():
     params = request.get_json()
-    cm = CurveEngine(data=params)
-    results = {}
-    for curveName, curve in cm.curves.items():
-        try:
-            curveNodes = [{'date': str(tup[0]), 'value': tup[1]}
-                          for tup in curve.nodes()]
-            results[curveName] = curveNodes
-        except Exception as e:
-            raise e
-    return results
+    try:
+        cm = ce.CurveEngine(params)
+        bootstrapResults = []
+        for curveName, curve in cm.curves.items():
+            curveResults = {}
+            nodes = [{'date': ce.parseOREDate(tup[0]), 'value': tup[1]}
+                     for tup in curve.nodes()]
+            curveResults['curveName'] = curveName
+            curveResults['curveConfig'] = {
+                'curveType': 'Discount',
+                'dayCounter': 'Actual360',
+                'enableExtrapolation': True,
+                'currency': getCurrency(params, curveName),
+                'nodes': nodes,
+            }
+            curveResults['curveIndex'] = getIndex(params, curveName)
+            bootstrapResults.append(curveResults)
+    except Exception as e:
+        return {'statusCode': 400, 'data': formatNestedException(e)}
+
+    results = {
+        'curveSetName': params['curveSetName'],
+        'refDate': params['refDate'],
+        'curves': bootstrapResults
+    }
+
+    return {'statusCode': 200, 'data': results}
+
+
+def getCurrency(config: dict, lookupCurve: str):
+    for curve in config['curves']:
+        if curve['curveName'] == lookupCurve:
+            return curve['curveConfig']['currency']
+
+
+def getIndex(config: dict, lookupCurve: str):
+    for curve in config['curves']:
+        if curve['curveName'] == lookupCurve:
+            return curve['curveIndex']
+
+
+def formatNestedException(e: Exception) -> str:
+    msg = []
+    level = 0
+    while e.__cause__ is not None:
+        level += 1
+        msg.append('Level ' + str(level) + ': ' + str(e))
+        e = e.__cause__
+    msg.append('Level ' + str(level + 1) + ': ' + str(e))
+    return msg
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
